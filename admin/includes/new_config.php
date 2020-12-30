dd39c7d07ba35021e5f6ad9ddc3c1599f351ab85
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2020-12-30 14:08:34 +0100

    fix some bug

--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -1,17 +1,17 @@
 <?php
 
 // DEVELOPMENT
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','gallery_db');
+// define('DB_HOST','localhost');
+// define('DB_USER','root');
+// define('DB_PASS','');
+// define('DB_NAME','gallery_db');
 
 
 // REWMOTE DATABASE CONNECTION
-// define('DB_HOST','remotemysql.com');
-// define('DB_USER','TxclVTrRqH');
-// define('DB_PASS','NqOLlXGkaL');
-// define('DB_NAME','TxclVTrRqH');
+define('DB_HOST','remotemysql.com');
+define('DB_USER','TxclVTrRqH');
+define('DB_PASS','NqOLlXGkaL');
+define('DB_NAME','TxclVTrRqH');
 
 
 ?>
